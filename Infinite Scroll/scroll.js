ea5b3df007c67501ed8202a875e0965005d32b7c
Rion Sawabe <53813654+sawabe0912@users.noreply.github.com>
2021-08-11 22:29:05 +0900

    Add files via upload

---
+++ Infinite Scroll/scroll.js
@@ -0,0 +1,78 @@
+const imageContainer = document.getElementById('picContainer');
+const loader = document.getElementById('loader');
+
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
+let photosArray = [];
+
+
+const count = 30;
+
+const apiKey = '';
+const apiUrl = `https://api.unsplash.com/photos/?client_id=${apiKey}&count=${count}`;
+
+
+function imageLoaded() {
+  imagesLoaded++;
+  if (imagesLoaded === totalImages) {
+    ready = true;
+    loader.hidden = true;
+  }
+}
+
+
+function setAttributes(element, attributes) {
+  for (const key in attributes) {
+    element.setAttribute(key, attributes[key]);
+  }
+}
+
+
+function displayPhotos() {
+  imagesLoaded = 0;
+  totalImages = photosArray.length;
+
+  photosArray.forEach((photo) => {
+
+    const item = document.createElement('a');
+    setAttributes(item, {
+      href: photo.links.html,
+      target: '_blank',
+    });
+
+    const img = document.createElement('img');
+    setAttributes(img, {
+      src: photo.urls.regular,
+      alt: photo.alt_description,
+      title: photo.alt_description,
+    });
+
+    img.addEventListener('load', imageLoaded);
+  
+    item.appendChild(img);
+    imageContainer.appendChild(item);
+  });
+}
+
+
+async function getPhotos() {
+  try {
+    const response = await fetch(apiUrl);
+    photosArray = await response.json();
+    displayPhotos();
+  } catch (error) {
+    console.log("Error");
+  }
+}
+
+
+window.addEventListener('scroll', () => {
+  if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready) {
+    ready = false;
+    getPhotos();
+  }
+});
+
+
+getPhotos();
